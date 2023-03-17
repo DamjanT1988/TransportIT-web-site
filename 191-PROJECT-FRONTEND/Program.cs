98667460728d6afb4727c20c22bbf1073c0b5e37
DamjanT1988 <Damjan_t@hotmail.com>
2023-03-17 14:36:17 +0100

    1

--- 191-PROJECT-FRONTEND/Program.cs
+++ 191-PROJECT-FRONTEND/Program.cs
@@ -2,10 +2,15 @@ using _191_PROJECT_FRONTEND;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+//create the builder
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+//build the app component
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//initailize http client service
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//wait and run app
 await builder.Build().RunAsync();
